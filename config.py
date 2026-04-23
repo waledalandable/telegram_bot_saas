d660876d93d03e3cd581aa7e208b020af146ef15
waledalandable <156304076+waledalandable@users.noreply.github.com>
2026-04-23 11:07:52 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "123456789"))
+BOT_TOKEN = os.getenv("7787809968:AAHpKbErc_GmNfFaK0uLmlD__4taeNojSLQ")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7496673554"))
 
-API_ID = int(os.getenv("API_ID", "12345"))
-API_HASH = os.getenv("API_HASH")
+API_ID = int(os.getenv("API_ID", "26310454"))
+API_HASH = os.getenv("41e23da71019b8f6de44e601d029e6e7")
 
 PLANS = {
     "basic": {"price": "10$", "limit": 100},
